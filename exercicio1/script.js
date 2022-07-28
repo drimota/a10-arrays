ef4e1c707fe5dd1b192382fbe5a2e3001963c819
drimota <driismota@hotmail.com>
2022-07-27 20:59:59 -0400

    exercicio 1 completo

---
+++ exercicio1/script.js
@@ -0,0 +1,17 @@
+//- O primeiro deve possuir apenas **números**, e estes números não devem seguir nenhuma ordem específica;
+
+const array = [90, -4, 28, 45, -92]
+
+// - O segundo array deve possuir apenas **strings**;
+
+const array1 = ["queijo", "chocolate", "morango", "quesadilla"]
+
+// - O terceiro array deve possuir **números**, **strings** e **booleanos**
+
+const array2 = [85, "oxente", false]
+
+// Imprima cada um dos arrays acima, utilizando `console.log()`
+
+console.log(array)
+console.log(array1)
+console.log(array2)
